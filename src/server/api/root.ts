ccb746678952745533030747205471f8a625ca5f
15077693d <oscaryiu.lapsang@gmail.com>
2023-09-17 09:02:58 +0800

    feat: update onsuccess logic to invaildate

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: shoppingItemRouter,
+  shoppingItem: shoppingItemRouter,
 });
 
 // export type definition of API
